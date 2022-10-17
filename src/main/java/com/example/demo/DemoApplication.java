fe7a1b531285f32dbb43456399ebf270221cb801
280947706 <83458548+280947706@users.noreply.github.com>
2022-10-17 11:51:15 +0800

    Mr.LI
    在网址的第二次修改

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,8 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         System.out.println("1111");
-        System.out.println("222222");
+        System.out.println("1111");
+        System.out.println("1111");
         SpringApplication.run(DemoApplication.class, args);
     }
 
